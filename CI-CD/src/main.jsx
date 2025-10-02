aeefebf0ca2945aa63aae02e35941297ca54165e
tyfld <tyfld@github.com>
2025-10-02 16:20:10 +0200

    modification

--- CI-CD/src/main.jsx
+++ CI-CD/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://eb805740b916db0498db01af738740b7@o4510120290025483.ingest.de.sentry.io/4510120291532880",
+  sendDefaultPii: true
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
